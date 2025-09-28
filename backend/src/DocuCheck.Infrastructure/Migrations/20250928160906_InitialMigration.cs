2c18b44b6ef4681e6db1fc4213038aefea43866a
Daniel Borek <daniel.borek64@gmail.com>
2025-09-28 19:41:19 +0200

    Adding InitialMigration to git

---
+++ backend/src/DocuCheck.Infrastructure/Migrations/20250928160906_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocuCheck.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CheckedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Number = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    CheckResult_ResultType = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckResult_Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckResult_Message = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckHistory", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckHistory");
+        }
+    }
+}
