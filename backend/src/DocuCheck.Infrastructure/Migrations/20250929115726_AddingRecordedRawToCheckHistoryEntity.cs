74dcf009b8f417de3263423e1d12b0c2d74e905a
Daniel Borek <daniel.borek64@gmail.com>
2025-09-29 14:43:17 +0200

    Add RecordedAtRaw property to CheckHistory entity and create migration

---
+++ backend/src/DocuCheck.Infrastructure/Migrations/20250929115726_AddingRecordedRawToCheckHistoryEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocuCheck.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingRecordedRawToCheckHistoryEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CheckedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Number = table.Column<string>(type: "TEXT", maxLength: 15, nullable: false),
+                    CheckResult_ResultType = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckResult_Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckResult_Message = table.Column<string>(type: "TEXT", nullable: true),
+                    CheckResult_RecordedAtRaw = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckHistory", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckHistory");
+        }
+    }
+}
